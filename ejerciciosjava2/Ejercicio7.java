6e0883dbb4210e6ea36a77543d5a1d20ab1f50c5
camposaracely313-droid <camposaracely313@gmail.com>
2026-07-19 09:28:01 -0500

    Create Ejercicio7.java

---
+++ ejerciciosjava2/Ejercicio7.java
@@ -0,0 +1,24 @@
+package com.mycompany.ejerciciosjava;
+import java.util.Scanner;
+/**
+ *
+ * @author aracely mabel campos cabrera
+ */
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matriz = new int[4][4];
+        int suma = 0;
+        
+        System.out.println("Ingrese la matriz 4x4:");
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print("Fila " + i + " Columna " + j + ": ");
+                matriz[i][j] = sc.nextInt();
+                suma += matriz[i][j]; // vamos sumando
+            }
+        }
+        
+        System.out.println("\nLa suma total de todos los elementos es: " + suma);
+    }
+}
