ad1ea4b4381121740c17435a73af605ee4a0d8ea
camposaracely313-droid <camposaracely313@gmail.com>
2026-07-19 09:27:17 -0500

    Create Ejercicio6.java

---
+++ ejerciciosjava2/Ejercicio6.java
@@ -0,0 +1,28 @@
+package com.mycompany.ejerciciosjava;
+import java.util.Scanner;
+/**
+ *
+ * @author aracely mabel campos cabrera
+ */
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matriz = new int[3][4];
+        
+        System.out.println("Ingrese la matriz 3x4:");
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print("Fila " + i + " Columna " + j + ": ");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        
+        System.out.println("\nMatriz ingresada:");
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 4; j++){
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+     }
