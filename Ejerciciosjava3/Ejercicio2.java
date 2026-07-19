75740d40a9ebd187df7c54074290990219f74ed1
camposaracely313-droid <camposaracely313@gmail.com>
2026-07-19 15:31:42 -0500

    Create Ejercicio2.java

---
+++ Ejerciciosjava3/Ejercicio2.java
@@ -0,0 +1,16 @@
+package com.mycompany.ejerciciosjava3;
+/**
+ *
+ * @author Aracely Mabel Campos Cabrera
+ */
+public class Ejercicio2 {
+    public static void mostrarPares(){
+        for(int i = 2; i <= 100; i += 2){
+            System.out.print(i + " ");
+        }
+    }
+    
+    public static void main(String[] args) {
+        mostrarPares();
+    }
+}
