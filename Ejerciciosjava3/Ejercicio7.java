aeab94de2ca431cf6c058cb4b791bd90f55ba477
camposaracely313-droid <camposaracely313@gmail.com>
2026-07-19 15:35:35 -0500

    Create Ejercicio7.java

---
+++ Ejerciciosjava3/Ejercicio7.java
@@ -0,0 +1,15 @@
+package com.mycompany.ejerciciosjava3;
+/**
+ *
+ * @author Aracely Mabel Campos Cabrera
+ */
+public class Ejercicio7 {
+    public static double obtenerPI(){
+        return 3.1416;
+    }
+    
+    public static void main(String[] args) {
+        double pi = obtenerPI();
+        System.out.println("El valor de PI es: " + pi);
+    }
+}
