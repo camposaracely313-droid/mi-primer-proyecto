ef4a4b5aff3602ad9c2d1fbba6eab8f4ac99dea4
camposaracely313-droid <camposaracely313@gmail.com>
2026-07-19 09:28:41 -0500

    Create Ejercicio8.java

---
+++ ejerciciosjava2/Ejercicio8.java
@@ -0,0 +1,28 @@
+package com.mycompany.ejerciciosjava;
+import java.util.Scanner;
+/**
+ *
+ * @author aracely mabel campos cabrera
+ */
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] matriz = new int[4][4];
+        int sumaDiagonal = 0;
+        
+        System.out.println("Ingrese la matriz 4x4:");
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 4; j++){
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        
+        System.out.println("\nElementos de la diagonal principal:");
+        for(int i = 0; i < 4; i++){
+            System.out.print(matriz[i][i] + " ");
+            sumaDiagonal += matriz[i][i];
+        }
+        
+        System.out.println("\nSuma de la diagonal principal: " + sumaDiagonal);
+    }
+}
