95c98c16e0d3a3a1726d5f461ce867a6cfbb2fb0
camposaracely313-droid <camposaracely313@gmail.com>
2026-07-19 15:30:59 -0500

    Create Ejercicio1.java

---
+++ Ejerciciosjava3/Ejercicio1.java
@@ -0,0 +1,17 @@
+package com.mycompany.ejerciciosjava3;
+/**
+ *
+ * @author Aracely Mabel Campos Cabrera
+ */
+public class Ejercicio1 {
+    // METODO
+    public static void bienvenida(){
+        System.out.println("Universidad Tecnica de Machala");
+        System.out.println("Tecnicas de Programacion");
+        System.out.println("Aracely Mabel Campos Cabrera");
+    }
+    
+    public static void main(String[] args) {
+        bienvenida(); // Llamar al metodo
+    }
+}
